a04a88807ac51d779ab9ef9a963848c240857995
Hua-Shan Wu <greenballow@gmail.com>
2020-04-15 05:29:53 +0800

    M

--- 3_1/2graphs.py
+++ 3_1/2graphs.py
@@ -2,38 +2,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 import serial
 import time
-Fs = 128.0;  # sampling rate
+Fs = 1024.0;  # sampling rate
 Ts = 1.0/Fs; # sampling interval
 t = np.arange(0,10,Fs) # time vector; create Fs samples between 0 and 1.0 sec.
 y1 = np.arange(-1.5,1.5,Ts) # signal vector; create Fs samples
 y2 = np.arange(-1.5,1.5,Ts)
 y3 = np.arange(-1.5,1.5,Ts)
 y4 = np.arange(-0.5,1.5,Ts)
+y1k = np.arange(-1.5,1.5,Ts)
+y2k = np.arange(-1.5,1.5,Ts)
+y3k = np.arange(-1.5,1.5,Ts)
+y4k = np.arange(-1.5,1.5,Ts)
 
 serdev = '/dev/ttyACM0'
 s = serial.Serial(serdev,115200)
 for x in range(0, int(Fs)):
     line=s.readline() # Read an echo string from K66F terminated with '\n'
     print(line)
-    y1,y2,y3,y4=line.split(" ",3)
-    print(y1)
-    print(y2)
-    print(y3)
-    print(y4)
-    y1[x] = float(y1)
-    y2[x] = float(y2)
-    y3[x] = float(y3)
-    y4[x] = float(y4)
+    y1=line.decode().strip().split(" ")[0]
+    y1k[x] = float(y1)
+    y2=line.decode().strip().split(" ")[1]
+    y2k[x] = float(y1)
+    y3=line.decode().strip().split(" ")[2]
+    y3k[x] = float(y3)
+    y4=line.decode().strip().split(" ")[3]
+    y4k[x] = float(y4)
 
 
 fig, ax = plt.subplots(2, 1)
-ax[0].plot(y1,'r',label = 'x')
-ax[0].plot(y2,'b',label = 'y')
-ax[0].plot(y3,'g',label = 'z')
+ax[0].plot(t,y1k,'r',label = 'x')
+ax[0].plot(t,y2k,'b',label = 'y')
+ax[0].plot(t,y3k,'g',label = 'z')
 ax[0].plt.legend(loc='lower left')
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Acc Vector')
-ax[1].plot(t,y4,'bo') # plotting the spectrum
+ax[1].plot(t,y4k,'bo') # plotting the spectrum
 ax[1].set_xlabel('Time')
 ax[1].set_ylabel('Tilt')
 plt.show()
